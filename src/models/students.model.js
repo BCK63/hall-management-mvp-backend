fef99a74d7dd48f8b437bbd1539306b53e968c51
ABIJOSEPHGEORGE <abijosephgeorge@gmail.com>
2023-05-25 00:43:31 +0530

    Added batch field in student schema

--- src/models/students.model.js
+++ src/models/students.model.js
@@ -16,6 +16,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  batch:{
+    type:String,
+    required:true,
+  },
 });
 
 export default model('students', userSchema);
