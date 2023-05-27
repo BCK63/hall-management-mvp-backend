e2ccf0ec448b99c32d640c6ef12c84281e9ade36
ABIJOSEPHGEORGE <abijosephgeorge@gmail.com>
2023-05-27 21:46:45 +0530

    feat: completed student signup & login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,25 @@
-import { studentLogin } from '../services/auth.services.js';
+import { studentLogin, studentSignup } from '../services/auth.services.js';
 import BadRequest from '../utils/errors/badRequest.js';
 import catchAsync from '../utils/errors/catchAsync.js';
 import ok from '../utils/responses/ok.js';
-import schema from '../utils/validations/login.schema.js';
+import schema from '../utils/validations/auth.schema.js';
 
 export const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const { error, value } = schema.loginSchema.validate({ email, password });
   if (error) throw new BadRequest(error.details[0].message);
-  const student = await studentLogin(value);
-  ok(res, 'login successful', student);
+  const token = await studentLogin(value);
+  ok(res, 'login successful', token);
 });
 
-export const signup = {};
+export const signup = catchAsync(async (req, res) => {
+  const {
+    name, email, password, batch,
+  } = req.body;
+  const { error, value } = schema.signupSchema.validate({
+    name, email, password, batch,
+  });
+  if (error) throw new BadRequest(error.message);
+  const student = await studentSignup(value);
+  ok(res, 'signup successfull', student);
+});
--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,5 +1,6 @@
 import studentsModel from '../models/students.model.js';
 
 export const findByStudentByEmail = async (email) => studentsModel.findOne({ email });
+export const createNewStudent = async (student) => studentsModel.create(student);
 
 export const sample = {};
