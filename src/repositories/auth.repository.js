70328a5c2490276ed3f8d5dcf17f3eb0c901c944
Junaid <junaidofficialnow@gmail.com>
2023-05-31 00:07:45 +0530

    changes

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -2,5 +2,3 @@ import studentsModel from '../models/students.model.js';
 
 export const findStudentByEmail = async (email) => studentsModel.findOne({ email });
 export const createNewStudent = async (student) => studentsModel.create(student);
-
-export const sample = {};
